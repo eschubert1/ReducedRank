b2cbbe8b8ce70dd104b5f1858d857575e31b4969
Ethan Schubert <egschub@gl-login3.arc-ts.umich.edu>
2025-08-25 08:53:47 -0400

    Adding Reduced Rank simulation files

---
+++ rr_gee.jl
@@ -0,0 +1,351 @@
+using StableRNGs
+using EstimatingEquationsRegression
+using Statistics
+using LinearAlgebra
+using Distributions
+
+import GLM
+using GLM: Link, LinPredModel, LinPred, ModResp, mueta, linkinv
+
+include("rr_sims.jl")
+
+# Copied from EstimatingEquationsRegression.jl
+const FP = AbstractFloat
+const FPVector{T<:FP} = AbstractArray{T,1}
+include("../EstimatingEquationsRegression.jl/src/linpred.jl")
+include("../EstimatingEquationsRegression.jl/src/varfunc.jl")
+
+# Copied from EstimatingEquationsRegression.jl
+function updateD!(p::EstimatingEquationsRegression.DensePred, dμdη::FPVector, i1::Int, i2::Int)
+    p.D = Diagonal(dμdη) * p.X[i1:i2, :]
+end
+
+geevar(d::Normal, v::EstimatingEquationsRegression.DefaultVar, mu::T, awt::T) where {T<:Real} = 1 / awt
+
+function covsolve(c::IndependenceCor, mu, sd, z)
+    return z ./ sd.^2
+end
+
+# Generate data for a multivariate GEE with given mean coefficients
+function mgeedata(nobs, ngroup, m, pm, pv, rank;rng=StableRNG(1))
+ N = nobs*ngroup
+
+ # Residual correlation for each outcome, each element sampled from U(-1,1)
+ rr = 2 .* rand(rng, m) .- 1
+
+ # Coefficients for mean model
+ Bm = genrr(m, p, r)
+ # Coefficients for scale model
+ Bv = randn(rng, pv, m)
+
+ g = repeat(1:nobs, inner=ngroup)
+ Xm = randn(rng, N, pm)
+ Xv = randn(rng, N, pv)
+ Xv[:, 1] .= 1
+ Xr = randn(rng, N, 2)
+
+ Ey = Xm * Bm
+ Vy = exp.(Xv * Bv)
+ clamp!(Vy, 0.1, 20)
+
+ # Generate additive errors with both row and column correlations.  The strength of the
+ # correlations is controlled by the parameter 'f'.
+ f = 0.5
+ E = zeros(N, m)
+ v = repeat(randn(nobs), inner=ngroup)
+ ee = randn(N)
+ for j in 1:m
+    u = repeat(randn(nobs), inner=ngroup)
+    u = sqrt(f)*v + sqrt(1-f)*u
+    e = sqrt(f)*ee + sqrt(1-f)*randn(N)
+    E[:, j] = sqrt.(Vy[:, j]) .* (sqrt(rr[j])*u + sqrt(1-rr[j])*e)
+ end
+
+ y = Ey + E
+
+ y, Bm, Bv, Xm, Xv, Xr, g
+end
+
+# Generate data for a multivariate GEE with reduced rank mean coefficients
+function mgeedata(nobs, ngroup, m, pm, pv, rank, Bm;rng=StableRNG(1), err_method=0)
+ N = nobs*ngroup
+
+ # Residual correlation for each outcome, each element sampled from U(0, 1)
+ rr = rand(rng, m)
+
+ # Coefficients for scale model
+ Bv = rand(rng, pv, m)
+
+ g = repeat(1:nobs, inner=ngroup)
+ Xm = randn(rng, N, pm)
+ Xv = randn(rng, N, pv)
+ Xv[:, 1] .= 1
+ Xr = randn(rng, N, 2)
+
+ Ey = Xm * Bm
+ Vy = exp.(Xv * Bv)
+ clamp!(Vy, 0.1, 10)
+
+ if err_method == 1
+ 	groups = repeat(1:nobs, inner=ngroup)
+	E = clustered_errors(groups, m)
+ else
+ 	# Generate additive errors with both row and column correlations.  The strength of the
+ 	# correlations is controlled by the parameter 'f'.
+ 	f = 0.5
+ 	E = zeros(N, m)
+ 	v = repeat(randn(nobs), inner=ngroup)
+ 	ee = randn(N)
+ 	for j in 1:m
+    		u = repeat(randn(nobs), inner=ngroup)
+    		u = sqrt(f)*v + sqrt(1-f)*u
+    		e = sqrt(f)*ee + sqrt(1-f)*randn(N)
+    		E[:, j] = sqrt.(Vy[:, j]) .* (sqrt(rr[j])*u + sqrt(1-rr[j])*e)
+ 	end
+ end
+
+ y = Ey + E
+
+ y, Bm, Bv, Xm, Xv, Xr, g
+end
+
+# Generate clustered errors with cluster membership defined by groups, and m is the number of response variables
+function clustered_errors(groups, m)
+ N = length(groups)
+ group_ids = unique(groups)
+ E = zeros(N, m)
+ for i in group_ids
+ 	ii = findall(j->(j==i), groups)
+ 	ng = length(ii)
+ 	C = Symmetric(gencov(ng*m))
+	A = cholesky(C)
+ 	Eg = A.L*randn(ng*m)
+	Eg = reshape(Eg, (ng, m))
+	E[ii, :] = Eg
+ end
+ E
+end
+
+# Determine Singular Angle Similarity between two matrices, A and B, of the same size
+function sas(A, B)
+ Asvd = svd(A)
+ Bsvd = svd(B)
+ m = min(size(A)[1], size(A)[2])
+ left1 = zeros(m)
+ right1 = zeros(m)
+ left2 = zeros(m)
+ right2 = zeros(m)
+ angles = zeros(m)
+ weights = zeros(m)
+ for i in 1:m
+ 	left1[i] = acos(clamp(Asvd.U[:, i]⋅Bsvd.U[:,i], -1, 1))
+	right1[i] = acos(clamp(Asvd.V[:, i]⋅Bsvd.V[:,i], -1, 1))
+	left2[i] = acos(clamp(-Asvd.U[:, i]⋅Bsvd.U[:,i], -1, 1))
+	right2[i] = acos(clamp(-Asvd.V[:, i]⋅Bsvd.V[:,i], -1, 1))
+ 	angles[i] = min((left1[i]+right1[i]), (left2[i]+right2[i]))
+	weights[i] = sqrt(Asvd.S[i]*Bsvd.S[i])
+ end
+ angles = angles ./ 2
+ delta = 1 .- angles ./ (pi/2)
+ sas = sum(weights .* delta)/sum(weights)
+ sas
+end
+
+# Fit multivariate response GEE2 (or GEE1) model and construct reduced rank estimator via WLRA.
+function mgee_rr(nobs, ngroup, m, pm, pv, rank, Bm, xlog; rng=StableRNG(1), method=2)
+ N = nobs*ngroup
+ write(xlog, "Generating data... \n")
+ y, Bm, Bv, Xm, Xv, Xr, g = mgeedata(nobs, ngroup, m, pm, pv, rank, Bm; rng=rng)
+
+ function make_rcov(x1, x2)
+    return [1., x1[2]+x2[2], abs(x1[2]-x2[2])]
+ end
+ 
+ if method == 2
+	mm = Array{Union{Nothing, GeneralizedEstimatingEquations2Model}}(nothing, m)
+ else
+ 	mm = Array{Union{Nothing, GeneralizedEstimatingEquationsModel}}(nothing, m)
+ end
+ 
+ write(xlog, "Fitting GEE2 models... \n")
+ for j in 1:m
+ 	if method == 2
+ 		mm[j] = fit(GeneralizedEstimatingEquations2Model, Xm, Xv, Xr, y[:, j], g, make_rcov; verbosity=0, cor_pair_factor=0.0)
+
+	else
+		mm[j] = fit(GeneralizedEstimatingEquationsModel, Xm, y[:,j], g; verbosity=0)
+	end
+ end
+ 
+ write(xlog, "Constructing covariance matrix... \n")
+ if method == 2
+ 	vc = vcov(mm...)
+ else
+ 	vc = zeros(pm*m, pm*m)
+	B = zeros(pm*m, pm*m)
+	for j in 1:m
+		 _iterprep(mm[j])
+    	end
+	for j in 1:m
+		jj = (1:pm) .+ pm*(j-1)
+		for k in (j+1):m
+			M = zeros(pm, pm)
+			jk = (1:pm) .+ pm*(k-1)
+			for l in 1:size(mm[j].rr.grpix, 2)
+        			# This should be the same for gee1 and gee2
+        			i1, i2 = mm[j].rr.grpix[:, l]
+
+        			_update_group(mm[j], l, false)
+        			_update_group(mm[k], l, false)
+
+       				# Update the meat for the mean parameters
+        			vc[jj, jj] .+= mm[j].pp.score_grp * mm[j].pp.score_grp'
+				M .+= mm[j].pp.score_grp * mm[k].pp.score_grp'
+			end
+			vc[jj, jk] = M
+			vc[jk, jj] = M'
+		end
+		B[jj, jj] = mm[j].cc.DtViD_sum
+	end
+	vc = B \ vc / B'
+ end
+
+ #mm1 = fit(GeneralizedEstimatingEquations2Model, Xm, Xv, Xr, y[:, 1], g, make_rcov;
+ #         verbosity=2, cor_pair_factor=0.0)
+ 
+ #mm2 = fit(GeneralizedEstimatingEquations2Model, Xm, Xv, Xr, y[:, 2], g, make_rcov;
+ #         verbosity=2, cor_pair_factor=0.0)
+
+ #mm3 = fit(GeneralizedEstimatingEquations2Model, Xm, Xv, Xr, y[:, 3], g, make_rcov;
+ #         verbosity=2, cor_pair_factor=0.0)
+
+ #vc = vcov(mm1, mm2, mm3)
+
+ # Print the correlations between parameter estimates for two responses.
+ #mc = vc[1]
+ #Diagonal(sqrt.(diag(mc[1:12, 1:12]))) \ mc[1:12, 13:24] / Diagonal(sqrt.(diag(mc[13:24, 13:24]))) |> diag |> display
+
+ b = Array{Union{Nothing, Vector}}(nothing, m)
+ for j in 1:m
+ 	if method == 2
+ 		b[j] = mm[j].mean_model.pp.beta0
+ 	else
+		b[j] = mm[j].pp.beta0
+	end
+ end
+ Bhat = hcat(b...)
+ #b1 = mm1.mean_model.pp.beta0
+ #b2 = mm2.mean_model.pp.beta0
+ #b3 = mm3.mean_model.pp.beta0
+
+ np = Int(div(size(vc[1], 1),m))
+ ii = Vector{Union{Nothing,UnitRange{Int64}}}(nothing, m)
+ for j in 1:m
+  ii[j] = (1:pm) .+ np*(j-1)
+ end
+ inds = vcat(ii...)
+ if method == 2
+ 	mc = vc[1][inds,inds]
+ else
+ 	mc = vc
+ end
+
+ write(xlog, "Computing Full WLRA... \n")
+ # Compute WLRA to Bhat using the estimated covariance of Bhat
+ Brr, Nrr = rr_sd(Bhat, mc, pm, m, rank; tol=1e-8)
+ 
+ # Compute truncated singular value decomposition of Bhat
+ Bsvd = tsvd(Bhat, rank)
+ Bsvd = Bsvd.U*Diagonal(Bsvd.S)*Bsvd.Vt
+
+ # Compute truncated singular value decomposition of fitted values
+ Ysvd = tsvd(Xm*Bhat, rank)
+ Ysvd = Ysvd.U*Diagonal(Ysvd.S)*Ysvd.Vt
+
+ write(xlog, "Computing Kronecker approximation... \n")
+ # Compute Kronecker approximation to estimated covariance matrix of Bhat
+ Ccol, Crow = kronapprox(mc, float.(Matrix(I, pm, pm)), m, m, pm, pm)
+ # Determine low-rank approximation weighted by estimated  row and column covariances
+ Bkron = rrkron(Bhat, Crow, Ccol, rank)
+
+ # Compute block covariance matrix of Bhat
+ Cblock = zeros(m*pm, m*pm)
+ for i in 1:m
+ 	inds = (1+pm*(i-1)):(pm*i)
+	Cblock[inds, inds] = mc[inds, inds]
+ end
+
+ write(xlog, "Computing Block WLRA... \n")
+ # Compute best WLRA based on Cblock
+ Bblock, Nblock = rr_sd(Bhat, Cblock, pm, m, rank; tol=1e-8)
+
+ # Estimate column covariance by taking trace of blocks of covariance of Bhat
+ Ctrace = zeros(m, m)
+ for i in 1:m
+ 	inds1 = (1+pm*(i-1)):(pm*i)
+	for j in i:m
+		inds2 = (1+pm*(j-1)):(pm*j)
+		Ctrace[i, j] = tr(mc[inds1, inds2])
+		Ctrace[j, i] = Ctrace[i, j]
+	end
+ end
+
+ # Compute truncated SVD of Bhat weighted by Ctrace
+ Ctr_1 = sqrt(inv(Ctrace))
+ Btr = tsvd(Bhat*Ctr_1, rank)
+ Btr = Btr.U*Diagonal(Btr.S)*Btr.Vt*sqrt(Ctrace)
+
+ # Compute truncated SVD of Yhat weighted by Ctrace
+ Ytr = tsvd(Xm*Bhat*Ctr_1, rank)
+ Ytr = Ytr.U*Diagonal(Ytr.S)*Ytr.Vt*sqrt(Ctrace)
+
+ write(xlog, "All estimators computed. \n\n")
+ # Compare distance of estimators to true coefficient matrix via Frobenius norm
+ R1 = sqrt(sum((Bhat-Bm).^2))
+ R2 = sqrt(sum((Brr-Bm).^2))
+ R3 = sqrt(sum((Bsvd-Bm).^2))
+ R4 = sqrt(sum(((Xm \ Ysvd)-Bm).^2))
+ R5 = sqrt(sum((Bkron-Bm).^2))
+ R6 = sqrt(sum((Bblock-Bm).^2))
+ R7 = sqrt(sum((Btr-Bm).^2))
+ R8 = sqrt(sum(((Xm \ Ytr)-Bm).^2))
+
+ # Compare distance of estimators to mean of response via Frobenius norm
+ F1 = sqrt(sum((Xm*(Bhat-Bm)).^2))
+ F2 = sqrt(sum((Xm*(Brr-Bm)).^2))
+ F3 = sqrt(sum((Xm*(Bsvd-Bm)).^2))
+ F4 = sqrt(sum((Ysvd - Xm*Bm).^2))
+ F5 = sqrt(sum((Xm*(Bkron-Bm)).^2))
+ F6 = sqrt(sum((Xm*(Bblock-Bm)).^2))
+ F7 = sqrt(sum((Xm*(Btr-Bm)).^2))
+ F8 = sqrt(sum((Ytr - Xm*Bm).^2))
+
+ # Compare differences of estimators using singular angle similarity
+ #R1 = sas(Bhat, Bm)
+ #R2 = sas(Brr, Bm)
+ #R3 = sas(Bsvd, Bm)
+
+ [R1, R2, R3, R4, R5, R6, R7, R8, F1, F2, F3, F4, F5, F6, F7, F8]
+ #Bm, Bhat, Bkron, mc, Crow, Ccol
+end
+
+function sim_gee(nobs, ngroup, m, pm, pv, rank, xlog;nsim=100, rng=StableRNG(1), method=2)
+ R = zeros(Float64, nsim, 16)
+ # Coefficients for mean model
+ Bm = genrr(pm, m, rank)
+ write(xlog, "Beginning simulation: \n")
+ for i in 1:nsim
+ 	write(xlog, string("Iteration #", i, ":\n"))
+ 	a = mgee_rr(nobs, ngroup, m, pm, pv, rank, Bm, xlog; rng=rng, method=method)
+	R[i,:] = a
+ end
+ write(xlog, string("Finished ", nsim, " iterations. \n"))
+ #out = round.(mean(R, dims=1); digits=8)
+ #println("Frobenius distance to true coefficient matrix:")
+ #println(["Dense GEE2", "Full WLRA", "Bhat SVD", "Yhat SVD", "Bhat KA", "Block WLRA", "Bhat CW", "Yhat CW"])
+ #println(out[1:8])
+ #println("Frobenius distance to mean of response:")
+ #println(["Dense GEE2", "Full WLRA", "Bhat SVD", "Yhat SVD", "Bhat KA", "Block WLRA", "Bhat CW", "Yhat CW"])
+ #println(out[9:16])
+ R, Bm
+end
